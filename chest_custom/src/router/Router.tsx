a9b6fe5f888cafea309c90422ed308fb8ef59535
GauthierCornec <gauthier.cornec@gmail.com>
2023-04-20 16:28:00 +0200

    add register & confirm account view

--- chest_custom/src/router/Router.tsx
+++ chest_custom/src/router/Router.tsx
@@ -3,12 +3,16 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../views/Home/Home";
 import Game from "../views/Game/Game";
 import Login from "../views/Login/Login";
+import RegisterCard from '../views/Register/Register';
+import ConfirmAccount from '../views/Register/ConfirmAccount';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />}/>
+                <Route path="/register" element={<RegisterCard />}/>
+                <Route path="/confirmAccount" element={<ConfirmAccount />}/>
                 <Route path="/game/:id" element={<Game />}/>
                 <Route path="/Login:" element={<Home />} />
             </Routes>
